e10147323abfa27f009a71965647d2d50739184a
maytinhdibo <iammaytinhdibo@gmail.com>
2020-08-26 23:22:00 +0700

    clean and component code

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,39 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const gfi = require('gulp-file-include');
+var bs = require('browser-sync').create();
+
+const layouts = [
+    './layouts/**/*'
+];
+
+const includeFiles = [
+    './pages/*.html'
+];
+
+const buildDir = './';
+
+// gulp.task('browser-sync', function () {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+// });
+
+gulp.task('include-html', function () {
+    return gulp.src(includeFiles)
+        .pipe(gfi())
+        .pipe(gulp.dest(buildDir)).pipe(bs.reload({ stream: true }));
+});
 
 gulp.task('sass', function () {
     return gulp.src("styles/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest('styles/css'))
+        .pipe(gulp.dest('styles/css')).pipe(bs.reload({ stream: true }));
 })
 
 gulp.task('watch', function () {
     gulp.watch('styles/scss/*.scss', gulp.series(['sass']));
+    gulp.watch(['pages/**/*.html', 'layouts/**/*.html'], gulp.series(['include-html']));
 });
--- js/dist/index.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function scrollBottom() {
-  window.scrollTo({
-    top: document.querySelector(".page.intro").offsetHeight,
-    left: 0,
-    behavior: 'smooth'
-  });
-}
