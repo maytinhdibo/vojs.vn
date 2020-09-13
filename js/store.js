093a2f3f1548fdafdf35443f890a7f70d5c1adcc
vuthaouet <vuthao.ak56ht@gmail.com>
2020-09-14 00:57:11 +0700

    fix demo

--- js/store.js
+++ js/store.js
@@ -1,7 +1,9 @@
 function demo(name, id) {
-    document.querySelector("#preview-demo .name").innerHTML = "Mã " + name;
-    document.querySelector("#preview-iframe").src = "/demo/demo" + id;
-    document.querySelector("#preview-demo").style.display = "block";
+    // document.querySelector("#preview-demo .name").innerHTML = "Mã " + name;
+    // document.querySelector("#preview-iframe").src = "/demo/demo" + id;
+    src = "https://vojs.vn/demo/demo" + id
+    window.open(src)
+    // document.querySelector("#preview-demo").style.display = "block";
 }
 document.querySelector("#close-preview").addEventListener("click", function () {
     document.querySelector("#preview-demo").style.display = "none";
