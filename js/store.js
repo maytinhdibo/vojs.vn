90f450f3103b933c7fc6c31dc4c67eb2f53950b6
maytinhdibo <iammaytinhdibo@gmail.com>
2020-09-13 23:21:30 +0700

    store.js

---
+++ js/store.js
@@ -0,0 +1,8 @@
+function demo(name, id) {
+    document.querySelector("#preview-demo .name").innerHTML = "Mã " + name;
+    document.querySelector("#preview-iframe").src = "/demo/" + id;
+    document.querySelector("#preview-demo").style.display = "block";
+}
+document.querySelector("#close-preview").addEventListener("click", function () {
+    document.querySelector("#preview-demo").style.display = "none";
+});
