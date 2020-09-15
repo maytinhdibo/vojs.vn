e6f6407e06b807522267fcfd2aceacd14f3e874f
vuthaouet <vuthao.ak56ht@gmail.com>
2020-09-15 17:07:10 +0700

    add theme store

--- js/store.js
+++ js/store.js
@@ -1,10 +1,19 @@
-function demo(name, id) {
+const themeItems= document.querySelectorAll(".info-image");
+themeItems.forEach(themeItem =>{
+    nameTheme = themeItem.getAttribute('name');
+    path = "/img/img_theme/"+nameTheme+".png";
+    url = "url('"+ path +"')";
+    themeItem.style.backgroundImage = url;
+    console.log(themeItem);
+    console.log(url);
+})
+function demo(ItemTheme) {
     // document.querySelector("#preview-demo .name").innerHTML = "Mã " + name;
     // document.querySelector("#preview-iframe").src = "/demo/demo" + id;
-    src = "https://vojs.vn/demo/demo" + id
-    window.open(src)
+    // window.open(herf)
     // document.querySelector("#preview-demo").style.display = "block";
+    window.open(ItemTheme)
 }
-document.querySelector("#close-preview").addEventListener("click", function () {
-    document.querySelector("#preview-demo").style.display = "none";
-});
+// document.querySelector("#close-preview").addEventListener("click", function () {
+//     document.querySelector("#preview-demo").style.display = "none";
+// });
